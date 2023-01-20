8eb48428168256396b179e7cef94c79f48dae2b5
Xi Fu <fuxialexander@gmail.com>
2023-01-20 12:56:09 -0500

    enformer based seq2motif

--- seq2motif/training.py
+++ seq2motif/training.py
@@ -3,17 +3,15 @@
 # 1. Given a list of regions and a genome assembly, generate a list of sequences (one-hot encoded numpy array) as inputs
 # 2. Given a list of sequences, generate a list of motif scores as targets
 #%%
-import pandas as pd
 import sys; sys.path.append('..')
+import numpy as np
+import pandas as pd
 import torch
-from tqdm import tqdm
 import torch.nn as nn
 import torch.nn.functional as F
-
-from atac_rna_data_processing.models.seq2motif import Seq2Motif
-
-import numpy as np
-from scipy.sparse import load_npz, save_npz, vstack, coo_matrix, csr_matrix
+from scipy.sparse import coo_matrix, csr_matrix, load_npz, save_npz, vstack
+from tqdm import tqdm
+from atac_rna_data_processing.models.enformer import Seq2Motif
 # %%
 def save_sparse_pandas_df(df, filename):
     data = df.sparse.to_coo()
@@ -24,24 +22,20 @@ def save_sparse_pandas_df(df, filename):
     np.save(filename + '.index.npy', index)
     return
 
-
 def load_sparse_pandas_df(filename):
     data = load_npz(filename+'.npz')
     columns = np.load(filename + '.columns.npy', allow_pickle=True)
     index = np.load(filename + '.index.npy', allow_pickle=True)
     return pd.DataFrame.sparse.from_spmatrix(data, index=index, columns=columns)
 
-
 def save_input(input, filename):
     save_npz(filename, vstack(input))
     return
-
 
 def save_dataset(input, target, filename):
     save_input(input, filename + '_data')
     save_sparse_pandas_df(target, filename + '_target')
     return
-
 
 def load_dataset(filename):
     target = load_sparse_pandas_df(filename + '_target')
@@ -62,10 +56,15 @@ def train(model, train_loader, optimizer, criterion, device):
     model.train()
     train_loss = 0
     for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
+        # evaluate every 100 batches
+        if batch_idx % 100 == 0:
+            test_loss = evaluate(model, test_loader, criterion, device)
+            print('test_loss: ', test_loss)
+            
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target.to_dense())
+        output = model(data)['motif']
+        loss = criterion(output.flatten(), target.to_dense().flatten())
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
@@ -79,20 +78,18 @@ def evaluate(model, test_loader, criterion, device, return_output=False):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += criterion(output, target.to_dense()).item()
+            output = model(data)['motif']
+            test_loss += criterion(output.flatten(), target.to_dense().flatten()).item()
     test_loss /= len(test_loader.dataset)
     if return_output:
         return test_loss, output.cpu().numpy(), target.to_dense().cpu().numpy()
     return test_loss
-
-
-
+#%%
 # dataloader
 class MotifDataset(torch.utils.data.Dataset):
     def __init__(self, data, target):
         self.data = data
-        self.target = target.sparse.to_coo().tocsr()
+        self.target = csr_matrix(target.sparse.to_dense())
         self.label = target.index.values
         self.motifs = target.columns.values
 
@@ -133,6 +130,9 @@ def sparse_batch_collate(batch:list):
     else:
         targets_batch = torch.FloatTensor(targets_batch)
     return data_batch, targets_batch
+
+
+
 #%%
 # set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -146,92 +146,21 @@ embedding_dim = 128
 num_heads = 4
 num_layers = 1
 dropout_rate = 0.5
-batch_size = 128
+batch_size = 256
 num_epochs = 100
-learning_rate = 0.001
+learning_rate = 0.0005
 # build dataloader
 train_dataset = MotifDataset(train_data, train_target)
 test_dataset = MotifDataset(test_data, test_target)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=sparse_batch_collate,drop_last=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,collate_fn=sparse_batch_collate,drop_last=True)
 #%%
-# build model
-
-# attention pooling layer
-class AttentionPooling(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(AttentionPooling, self).__init__()
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.fc1 = nn.Linear(input_dim, output_dim)
-        self.fc2 = nn.Linear(output_dim, 1)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        # x: (batch_size, seq_len, input_dim)
-        # attention: (batch_size, seq_len, 1)
-        attention = self.softmax(self.fc2(F.relu(self.fc1(x))))
-        # output: (batch_size, input_dim)
-        output = torch.sum(attention * x, dim=1)
-        return output
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, input_dim, output_dim, num_heads):
-        super(MultiHeadAttention, self).__init__()
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.num_heads = num_heads
-        self.fc1 = nn.Linear(input_dim, output_dim)
-        self.fc2 = nn.Linear(output_dim, 1)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        # x: (batch_size, seq_len, input_dim)
-        # attention: (batch_size, seq_len, 1)
-        attention = self.softmax(self.fc2(F.relu(self.fc1(x))))
-        # output: (batch_size, input_dim)
-        output = torch.sum(attention * x, dim=1)
-        return output
-    
-# seq2motif model
-class Seq2Motif(nn.Module):
-    def __init__(self, num_motifs, seq_len, embedding_dim, num_heads, num_layers, dropout_rate):
-        super(Seq2Motif, self).__init__()
-        self.num_motifs = num_motifs
-        self.seq_len = seq_len
-        self.embedding_dim = embedding_dim
-        self.num_heads = num_heads
-        self.num_layers = num_layers
-        self.dropout_rate = dropout_rate
-        self.conv1 = nn.Conv1d(in_channels=4, out_channels=embedding_dim, kernel_size=20, stride=1, padding=10)
-        self.conv2 = nn.Conv1d(in_channels=embedding_dim, out_channels=embedding_dim, kernel_size=10, stride=5, padding=5)
-        self.conv3 = nn.Conv1d(in_channels=embedding_dim, out_channels=embedding_dim, kernel_size=10, stride=5, padding=5)
-        self.conv4 = nn.Conv1d(in_channels=embedding_dim, out_channels=embedding_dim, kernel_size=5, stride=5, padding=2)
-        self.conv5 = nn.Conv1d(in_channels=embedding_dim, out_channels=embedding_dim, kernel_size=5, stride=5, padding=1)
-        # self.attention = MultiHeadAttention(input_dim=embedding_dim, output_dim=embedding_dim, num_heads=num_heads)
-        self.fc1 = nn.Linear(embedding_dim*2, num_motifs)
-        self.dropout = nn.Dropout(dropout_rate)
-        
-    def forward(self, x):
-        # x: (batch_size, seq_len, 4)
-        
-        x = self.dropout(F.relu(self.conv1(x)))
-        x = self.dropout(F.relu(self.conv2(x)))
-        x = self.dropout(F.relu(self.conv3(x)))
-        x = self.dropout(F.relu(self.conv4(x)))
-        x = self.dropout(F.relu(self.conv5(x)))
-        # x = self.attention(x)
-        x = self.fc1(x.to_dense().view(batch_size, -1))
-        return x
-    
-
-model = Seq2Motif(num_motifs, seq_len, embedding_dim, num_heads, num_layers, dropout_rate).to(device)
+model = Seq2Motif().to(device)
 # set optimizer and criterion
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.MSELoss()
 
 #%%
-
 # train model
 train_losses = []
 test_losses = []
@@ -253,10 +182,8 @@ np.save('test_losses.npy', np.array(test_losses))
 loss, target, output = evaluate(model, test_loader, criterion, device, return_output=True)
 # %%
 import seaborn as sns
-sns.scatterplot(x=target.reshape(-1), y=output.reshape(-1), s=1, alpha=0.5)
+
+sns.scatterplot(x=target[:, 0].reshape(-1), y=output[:, 0].reshape(-1), hue=np.repeat(train_dataset.motifs, 256, axis=0).reshape(-1, 256).T[:, 0].reshape(-1), s=10, alpha=1, legend=False)
 # %%
-output
-# %%
-sns.lineplot(x=np.arange(len(train_losses)), y=train_losses)
-sns.lineplot(x=np.arange(len(test_losses)), y=test_losses)
+torch.save(model.state_dict(), 'model.pth')
 # %%
