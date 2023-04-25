971af9a95aa53cbffddff2795ff3507bc980cc1c
Xi Fu <fuxialexander@gmail.com>
2023-04-25 15:50:26 -0400

    mutation from rsid and motif scanning with mutation

--- test/test_mutation.py
+++ test/test_mutation.py
@@ -2,11 +2,15 @@
 from atac_rna_data_processing.io.mutation import *
 from atac_rna_data_processing.io.region import *
 hg38 = Genome('hg38', '/home/xf2217/Projects/common/hg38.fa')
+from atac_rna_data_processing.io.nr_motif_v1 import NrMotifV1
+motif = NrMotifV1("/home/xf2217/Projects/motif_databases/motif-clustering/")
+# %%
 gbm_gwas = read_gwas_catalog(hg38, '/home/xf2217/Projects/geneformer_nova/analyze_gbm/gwas-association-downloaded_2023-04-25-EFO_0000519.tsv')
+
 # %%
-gbm_gwas = gbm_gwas.drop_duplicate_positions()
+gbm_gwas.get_motif_diff(motif)
 # %%
-gbm_gwas
+alt_motif, ref_motif = gbm_gwas.get_motif_diff(motif).values()
 # %%
-len(gbm_gwas.Ref_seq[0].sequences)
+alt_motif.values-ref_motif.values
 # %%
