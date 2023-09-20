962f4650b52b23b7ed337736b370f5d89209163c
Alejandro Buendia <alb2281@columbia.edu>
2023-09-20 20:02:42 +0000

    Add glob and open file with s3

--- atac_rna_data_processing/io/s3_utils.py
+++ atac_rna_data_processing/io/s3_utils.py
@@ -4,13 +4,26 @@ import s3fs
 import zarr
 import numpy as np
 from scipy.sparse import load_npz
+from glob import glob
 
+
+def open_file_with_s3(file_path, mode="r", s3_file_sys=None):
+    if s3_file_sys:
+        return s3_file_sys.open(file_path, mode=mode)
+    else:
+        return open(file_path, mode=mode)
 
 def path_exists_with_s3(file_path, s3_file_sys=None):
     if s3_file_sys:
         return s3_file_sys.exists(file_path)
     else:
         return os.path.exists(file_path)
+
+def glob_with_s3(file_path, s3_file_sys=None):
+    if s3_file_sys:
+        return s3_file_sys.glob(file_path)
+    else:
+        return glob(file_path)
 
 def load_zarr_with_s3(file_path, mode="r", s3_file_sys=None):
     if s3_file_sys:
