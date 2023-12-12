aea4ded8f2d8b329248f1d8df896eb8401d96f83
Alejandro Buendia <alb2281@columbia.edu>
2023-12-12 14:38:23 -0500

    Add driver script

---
+++ analysis/glioma_variant_scores.py
@@ -0,0 +1,47 @@
+"""
+Script for producing GET variant scores (motif difference * importance)
+Example script for glioma and MYC gene over all cell types
+"""
+import os
+from atac_rna_data_processing.io.mutation import CellMutCollection
+
+
+# Configuration
+working_dir = "/manitou/pmg/users/xf2217/interpret_natac/" # Directory with reference genome and motif file
+genome_path = os.path.join(working_dir, "hg38.fa")
+motif_path = os.path.join(working_dir, "NrMotifV1.pkl")
+
+model_ckpt_path = "/manitou/pmg/projects/resources/get_interpret/pretrain_finetune_natac_fetal_adult.pth" # Model ckpt
+get_config_path = "/manitou/pmg/users/xf2217/atac_rna_data_processing/atac_rna_data_processing/config/GET" # GET config
+
+celltype_annot_path = "/manitou/pmg/users/xf2217/pretrain_human_bingren_shendure_apr2023/data/cell_type_pretrain_human_bingren_shendure_apr2023.txt" # cell type mapping
+celltype_dir = "/manitou/pmg/users/xf2217/Interpretation_all_hg38_allembed_v4_natac/*" # If celltype list not provided, use this directory of all cell types
+
+normal_variants_path = "/manitou/pmg/users/xf2217/gnomad/myc.tad.vcf.gz" # gnomAD normal variants
+variants_map_path = "/manitou/pmg/users/xf2217/interpret_natac/glioma_variants.txt" # Variants with LD variants
+
+celltype_path = None # Cell types of interest
+variants_path = "/manitou/pmg/users/xf2217/interpret_natac/myc_rsid.txt" # Variants of interest
+genes_path = "/pmglocal/alb2281/repos/atac_rna_data_processing/analysis/genes.txt" # Genes of interest
+
+output_dir = "/pmglocal/alb2281/repos/atac_rna_data_processing/analysis/" # Output directory
+output_name = "glioma" # Experiment name
+num_workers = 10 # Number of workers for parallelization
+
+
+cell_mut_col = CellMutCollection(
+    model_ckpt_path=model_ckpt_path,
+    get_config_path=get_config_path,
+    genome_path=genome_path,
+    motif_path=motif_path,
+    celltype_annot_path=celltype_annot_path,
+    celltype_dir=celltype_dir,
+    celltype_path=celltype_path,
+    normal_variants_path=normal_variants_path,
+    variants_map_path=variants_map_path,
+    variants_path=variants_path,
+    genes_path=genes_path,
+    output_dir=output_dir,
+    num_workers=num_workers,
+)
+scores = cell_mut_col.get_all_variant_scores(output_name)
--- atac_rna_data_processing/io/mutation.py
+++ atac_rna_data_processing/io/mutation.py
@@ -363,6 +363,7 @@ class CellMutCollection(object):
             motif_path,
             celltype_annot_path,
             celltype_dir,
+            celltype_path,
             normal_variants_path,
             variants_map_path,
             variants_path,
@@ -373,7 +374,8 @@ class CellMutCollection(object):
         self.output_dir = output_dir
         celltype_annot = pd.read_csv(celltype_annot_path)
         self.celltype_annot_dict = celltype_annot.set_index('id').celltype.to_dict()
-        self.celltypes_list = [os.path.basename(cell_path) for cell_path in sorted(glob(celltype_dir))]
+        if celltype_path is None:
+            self.celltypes_list = [os.path.basename(cell_path) for cell_path in sorted(glob(celltype_dir))]
         with open(genes_path, "r") as f:
             self.genes_list = f.read().splitlines() 
         with open(variants_path, "r") as f:
@@ -523,40 +525,3 @@ class SVs(object):
         """
         pd.read_csv(self.bedpe_file, sep='\t', header=None)
         return
-
-
-if __name__=="__main__":
-    # Configuration files
-    working_dir = "/manitou/pmg/users/xf2217/interpret_natac/"
-    genome_path = os.path.join(working_dir, "hg38.fa")
-    motif_path = os.path.join(working_dir, "NrMotifV1.pkl")
-
-    model_ckpt_path = "/manitou/pmg/projects/resources/get_interpret/pretrain_finetune_natac_fetal_adult.pth"
-    get_config_path = "/manitou/pmg/users/xf2217/atac_rna_data_processing/atac_rna_data_processing/config/GET"
-
-    celltype_annot_path = "/manitou/pmg/users/xf2217/pretrain_human_bingren_shendure_apr2023/data/cell_type_pretrain_human_bingren_shendure_apr2023.txt"
-    celltype_dir = "/manitou/pmg/users/xf2217/Interpretation_all_hg38_allembed_v4_natac/*"
-
-    normal_variants_path = "/manitou/pmg/users/xf2217/gnomad/myc.tad.vcf.gz"
-    variants_map_path = "/manitou/pmg/users/xf2217/interpret_natac/glioma_variants.txt"
-    variants_path = "/manitou/pmg/users/xf2217/interpret_natac/myc_rsid.txt"
-    genes_path = "/pmglocal/alb2281/repos/atac_rna_data_processing/analysis/genes.txt"
-    output_dir = "/pmglocal/alb2281/repos/atac_rna_data_processing/analysis/"
-    output_name = "debug"
-    num_workers = 10
-    
-    cell_mut_col = CellMutCollection(
-        model_ckpt_path=model_ckpt_path,
-        get_config_path=get_config_path,
-        genome_path=genome_path,
-        motif_path=motif_path,
-        celltype_annot_path=celltype_annot_path,
-        celltype_dir=celltype_dir,
-        normal_variants_path=normal_variants_path,
-        variants_map_path=variants_map_path,
-        variants_path=variants_path,
-        genes_path=genes_path,
-        output_dir=output_dir,
-        num_workers=num_workers,
-    )
-    scores = cell_mut_col.get_all_variant_scores(output_name)
